74c8fc25b9716753342a0c9f57e8b5efd173aedb
javaclass <javaclass@15000537CPU.fec.org>
2017-09-26 15:21:20 -0500

    calculator

---
+++ src/basics1/Calculator.java
@@ -0,0 +1,18 @@
+package basics1;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.print(calculateSum(1000, 13));
+		// TODO Auto-generated method stub
+		}
+
+	static int calculateSum(int a, int b) {
+		return a+b; 
+	}
+		
+		
+	
+		}
+
+
