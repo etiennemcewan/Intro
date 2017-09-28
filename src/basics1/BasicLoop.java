846bd304edd32fba9a4ca111c1f6523486c94eae
javaclass <javaclass@15000537CPU.fec.org>
2017-09-28 13:10:35 -0500

    Forward and Reverse Loop

--- src/basics1/BasicLoop.java
+++ src/basics1/BasicLoop.java
@@ -5,16 +5,28 @@ public class BasicLoop {
 	public static void main(String[] args) {
 		
 		
-		for(int i = 10; i>=1; i--)
+		for(int i = 100; i>=1; i--)
 			System.out.println(i);
+		System.out.println("i love chicken");
+		System.out.println("poetry in motion");
+		System.out.println("without communication there is no love");
+			for(int t = 1; t<=100; t++) {
 			
 			
+			System.out.println(t);
 			
+			}
 			
 			
 		}
+}
+			
+			
+			
+			
+		
 		
 		
 
-	}
+	
 
