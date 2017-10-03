b531f8e30f5dfcf3a95e5a8f028c3544cb1d655f
javaclass <javaclass@15000537CPU.fec.org>
2017-10-03 09:58:26 -0500

    Sum of two numbers

---
+++ src/basics1/Sum.java
@@ -0,0 +1,23 @@
+package basics1;
+
+public class Sum {
+
+
+	public static void main(String[] args) {
+		
+		System.out.println(Sum(2, 3));
+		
+	}
+	
+	static int Sum(int a, int b) {
+
+		if ( a ==  b) {
+			return a * b;
+		}
+
+		else
+			return a+b;
+
+	}
+
+}
